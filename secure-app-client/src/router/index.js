09d1a0c6a161ee265fa04dc01eca03bfe2ac587a
KwanXinJie <xjkwan1998@gmail.com>
2020-10-23 17:59:37 +0800

    added server and deployed to aws

--- secure-app-client/src/router/index.js
+++ secure-app-client/src/router/index.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home'
 import Secure from '@/components/secure'
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-3424431.okta.com/oauth2/default',
+  client_id: '0oaeij5vkHoY0d27O5d5',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  scope: 'openid profile email'
+})
 
 Vue.use(Router)
 
@@ -14,11 +22,20 @@ let router = new Router({
       component: Home
     },
     {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
+    {
       path: '/secure',
       name: 'Secure',
-      component: Secure
+      component: Secure,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
 
 export default router
